a41c55a8b2181d1fe1c34cfab9ad88039558425f
Roshan <roshan.2201021cs@iiitbh.ac.in>
2024-03-23 19:57:50 +0530

    chore: testing

--- Easy/Day1/CQueue.h
+++ Easy/Day1/CQueue.h
@@ -1,13 +1,16 @@
-template <typename T> class CQueue
+template <typename T>
+class CQueue
 {
-	private: 
-		int front, back;
-		int length;
-		int *arr;
-	public:
-		CQueue(int s);				//constructor for circular queue object
-		void push(T value);			//insert element into circular queue
-		T pop();					//delete the element from the front of circular queue and return it
-		const &T front();			//return element present at the front of the queue
-        T size();                   //return number of elements in circular queue 
+private:
+	int front, back;
+	int length;
+	int *arr;
+
+public:
+	CQueue(int s);		// constructor for circular queue object
+	void push(T value); // insert element into circular queue
+	T pop();			// delete the element from the front of circular queue and return it
+	const &T front();	// return element present at the front of the queue
+	T size();			// return number of elements in circular queue
+	// this is for the testing;
 };
