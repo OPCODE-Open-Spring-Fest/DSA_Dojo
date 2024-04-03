0d5090f535619d48ee42ff04ca873edde2201526
ashutosh <ashutosh.2201062cs@iiitbh.ac.in>
2024-04-03 17:14:49 +0530

    chore: solution medium day 5 task

--- Medium/Day5/Solution.cpp
+++ Medium/Day5/Solution.cpp
@@ -6,37 +6,53 @@ using namespace std;
 
 
 
+int value(int m,int x,vector<int>& psum)
+{
+    auto sum=[&](int i,int j)
+    {
+        if (i==0 ) return psum[j];
+        else return psum[j]-psum[i-1];
+    };
+    int s=0,e=m;
+    int ans=0;
+    while (s<=e)
+    {
+        int mid=(s)+(e-s)/2;
+        int req=(m-mid+1)*x-sum(mid,m);
+        if (req<=mid)
+        {
+            ans=m-mid+1;
+            e=mid-1;
+        }
+        else {s=mid+1;}
+    }
+    return ans;
+
+}
+
+
+
 void solve ()
 {
    int n,q;
    cin>>n>>q;;
-   int a[n];
+   vector<int>a(n);
    for (int i = 0; i < n; i++)
    {
        cin>>a[i];
    }
-   sort (a,a+n);
+   sort (a.begin(),a.end());
+   vector<int> psum(a.begin(),a.end());
+   for(int i=1;i<n;i++)psum[i]+=psum[i-1];
     while (q--)
     {
         int x;
         cin>>x;
-        int l=0,cnt=0;
-        for (int i=n-1;i>=l;i--)
-        {
-            if (a[i]>=x)
-            {
-                cnt++;
-            }
-            else if (a[i]<x)
-            {
-                int p=x-a[i];
-                if (l+p<i)
-                {
-                    l+=p;
-                    cnt++;
-                }
-            }
-        }
+        int cnt=0;
+        
+        int end=upper_bound(a.begin(),a.end(),x-1)-a.begin();
+        cnt+=(n-end);
+        cnt+=value(end-1,x,psum);
         cout<<cnt<<endl;
     }    
     
