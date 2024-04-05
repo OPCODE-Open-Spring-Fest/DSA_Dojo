92c43027cd6d6b3709670c7bac94c14bc9a08c5c
Roshan <roshan.2201021cs@iiitbh.ac.in>
2024-04-05 09:22:01 +0530

    chore: day7 medium problem

--- Medium/Day6/Solution.cpp
+++ Medium/Day6/Solution.cpp
@@ -1,57 +1,46 @@
 // Write your code here
 #include <bits/stdc++.h>
-#include<atcoder/all>
+#include <atcoder/all>
 using namespace std;
 #define int long long int
 
-
 /*---------------------------------------------------------------------------------*/
 
+int op(int a, int b) { return a + b; }
+int e() { return 0; }
 
-
-int op(int a, int b){return a + b;}
-int e(){return 0;}
-
-void solve ()
+void solve()
 {
-   
-    int n,q;
-    cin>>n>>q;
-    vector <int> a(n);
+
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
     for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
     atcoder::segtree<int, op, e> st(a);
     for (int i = 0; i < q; i++)
     {
         int t;
-        cin>>t;
-        if (t==0){
-            int p,x;
-            cin>>p>>x;
-            st.set(p,st.get(p)+x);
+        cin >> t;
+        if (t == 0)
+        {
+            int p, x;
+            cin >> p >> x;
+            st.set(p, st.get(p) + x);
         }
-        else{
-            int l,r;
-            cin>>l>>r;
-            cout<<st.prod(l,r)<<endl;
-        
+        else
+        {
+            int l, r;
+            cin >> l >> r;
+            cout << st.prod(l, r) << endl;
         }
-
     }
-
-
 }
 
+signed main()
+{
 
-signed  main() {
-
-	
-        solve();
- 
-
-    
-
+    solve();
 }
-
