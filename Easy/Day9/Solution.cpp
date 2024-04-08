66f63e3ff9b60931ef069c54ed539a35987f5056
Akash2005sharma <akashsharma.cpp@gmail.com>
2024-04-09 00:41:18 +0530

    test(Easy): subject Day9

--- Easy/Day9/Solution.cpp
+++ Easy/Day9/Solution.cpp
@@ -1,1 +1,18 @@
-// Write your code here
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int num=n;
+    long long a=1,b=1;
+    while(n){
+        a*=n;
+        n--;
+    }
+    n=num-m;
+    while(n){
+        b*=n;
+        n--;
+    }
+    cout<<(a/b)%1000000007;
+}
