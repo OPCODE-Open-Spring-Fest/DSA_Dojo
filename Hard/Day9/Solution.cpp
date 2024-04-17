f856c0e452e981c16b8b1ce5ddd78925380224de
ashutosh <ashutosh.2201062cs@iiitbh.ac.in>
2024-04-17 18:48:28 +0530

    chore: solved hard day 9 task

--- Hard/Day9/Solution.cpp
+++ Hard/Day9/Solution.cpp
@@ -1,1 +1,28 @@
 // Write Your Code Here
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int cnt[30][2];
+int main(){
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		ll l,r;
+		scanf("%lld %lld",&l,&r);
+		memset(cnt,0,sizeof(cnt));
+		for(int i=l;i<=r;i++){
+			int x;
+			scanf("%d",&x);
+			for(int j=30;j>=0;j--){
+				int tmp=x>>j&1;
+				cnt[j][tmp]++;
+			}
+		} 
+		ll res=0;
+		for(int i=30;i>=0;i--){
+			if(cnt[i][1]>cnt[i][0]) res+=1<<i;
+		}
+		printf("%lld\n",res);
+	}
+	return 0;
+}
